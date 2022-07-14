11e06a18a1a693f8e90fdd9da4d5b61bff05612a
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-07-14 18:26:28 +0900

    current_user modify current_company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,13 @@ class ApplicationController < ActionController::Base
             ])
     end
     
+
+    def after_sign_in_path_for(resource)
+        case resource
+        when Company
+            companies_path
+        when User
+            root_path
+        end
+      end
 end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,6 +4,8 @@ class CompaniesController < ApplicationController
         @users = User.all
         @products = Product.all.sum(:price)
         @orders = Order.all.order(created_at: "DESC")
+        @testers = Tester.all.sum(:price)
+        @test_orders = TestOrder.all.order(created_at: "DESC")
         
     end
 
