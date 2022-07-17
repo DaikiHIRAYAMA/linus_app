cee8fc49bfbed2bade40a57064e2e8c9a82fff63
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-07-18 01:58:21 +0900

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
 
 
   def render_404
-    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html', formats: :html
   end
 
   def render_500
-    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html', formats: :html
   end
 
     before_action :configure_permitted_parameters, if: :devise_controller?
