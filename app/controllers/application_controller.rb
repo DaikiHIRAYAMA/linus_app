e3cdc93f8c352aec155b4fba772c35b8c5c863ba
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-07-18 02:20:23 +0900

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
 
+    rescue_from Exception, with: :render_500
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from Exception, with: :render_500
 
 
 
   def render_404
-    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html', formats: :html
+    render file: "#{Rails.root}/public/404.html", status: 404, layout: 'application', content_type: 'text/html', formats: :html
   end
 
   def render_500
-    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html', formats: :html
+    render  file: "#{Rails.root}/public/500.html", status: 500, layout: 'application', content_type: 'text/html', formats: :html
   end
 
     before_action :configure_permitted_parameters, if: :devise_controller?
