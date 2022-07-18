70c255fcc984bc9b95ba64002b17a5137e42864a
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-07-18 20:54:55 +0900

    error 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
     render file: "#{Rails.root}/public/404.html", status: 404, layout: 'application', content_type: 'text/html', formats: :html
   end
 
-  #def render_500
-  #  render  file: "#{Rails.root}/public/500.html", status: 500, layout: 'application', content_type: 'text/html', formats: :html
-  #end
+  def render_500
+    render  file: "#{Rails.root}/public/500.html", status: 500, layout: 'application', content_type: 'text/html', formats: :html
+  end
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
